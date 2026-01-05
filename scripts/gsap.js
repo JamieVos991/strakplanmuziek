b4b4c46009182b3e77956d1ff94363fe76d186c6
Jamie Vos <Jamievos100@gmail.com>
2026-01-05 21:57:31 +0100

    h1 on load fix

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -4,31 +4,40 @@ import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export function initGSAP() {
-  // Make h1 visible when GSAP is ready
-  gsap.set("h1", { visibility: "visible" });
+  gsap.fromTo(
+    "h1",
+    { y: 100, autoAlpha: 0 },
+    {
+      y: 0,
+      autoAlpha: 1,
+      duration: 1.2,
+      ease: "power4.out",
+    }
+  );
 
-  gsap.from("h1", {
-    y: 100,
-    opacity: 0,
-    duration: 1.2,
-    ease: "power4.out",
-  });
+  gsap.fromTo(
+    "h1 .h1-muziek",
+    { y: 100, autoAlpha: 0 },
+    {
+      y: 0,
+      autoAlpha: 1,
+      delay: 0.2,
+      duration: 0.8,
+      ease: "power3.out",
+    }
+  );
 
-  gsap.from("h1 .h1-muziek", {
-    y: 100,
-    opacity: 0,
-    delay: 0.2,
-    duration: 0.8,
-    ease: "power3.out",
-  });
-
-  gsap.from("h1 .h1-voor", {
-    y: 50,
-    opacity: 0,
-    delay: 0.4,
-    duration: 1,
-    ease: "power3.out",
-  });
+  gsap.fromTo(
+    "h1 .h1-voor",
+    { y: 50, autoAlpha: 0 },
+    {
+      y: 0,
+      autoAlpha: 1,
+      delay: 0.4,
+      duration: 1,
+      ease: "power3.out",
+    }
+  );
 
   gsap.from(
     "section:nth-of-type(3) p, section:nth-of-type(3) a, section:nth-of-type(3) h2",
@@ -39,12 +48,13 @@ export function initGSAP() {
         toggleActions: "play none none none",
       },
       y: 50,
-      opacity: 0,
+      autoAlpha: 0,
       duration: 1,
       ease: "power3.out",
     }
   );
 }
+
 
 
 export function initPopup() {

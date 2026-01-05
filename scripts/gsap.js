57efb87f986204821f1b0b55f77e04631ad66d41
Jamie Vos <Jamievos100@gmail.com>
2026-01-05 21:54:19 +0100

    h1 on load fix

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -4,34 +4,42 @@ import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export function initGSAP() {
-  gsap.set("h1", { autoAlpha: 0, y: 100 });
+  // Make h1 visible when GSAP is ready
+  gsap.set("h1", { visibility: "visible" });
 
-  gsap.to("h1", {
-    autoAlpha: 1,
-    y: 0,
+  gsap.from("h1", {
+    y: 100,
+    opacity: 0,
     duration: 1.2,
     ease: "power4.out",
   });
 
-  gsap.fromTo(
-    "h1 .h1-muziek",
-    { autoAlpha: 0, y: 100 },
-    {
-      autoAlpha: 1,
-      y: 0,
-      delay: 0.2,
-      duration: 0.8,
-      ease: "power3.out",
-    }
-  );
+  gsap.from("h1 .h1-muziek", {
+    y: 100,
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.8,
+    ease: "power3.out",
+  });
 
-  gsap.fromTo(
-    "h1 .h1-voor",
-    { autoAlpha: 0, y: 50 },
+  gsap.from("h1 .h1-voor", {
+    y: 50,
+    opacity: 0,
+    delay: 0.4,
+    duration: 1,
+    ease: "power3.out",
+  });
+
+  gsap.from(
+    "section:nth-of-type(3) p, section:nth-of-type(3) a, section:nth-of-type(3) h2",
     {
-      autoAlpha: 1,
-      y: 0,
-      delay: 0.4,
+      scrollTrigger: {
+        trigger: "section:nth-of-type(3) p",
+        start: "top 90%",
+        toggleActions: "play none none none",
+      },
+      y: 50,
+      opacity: 0,
       duration: 1,
       ease: "power3.out",
     }
